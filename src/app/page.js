d8ae08f3fa63d8e6be5388826791c8f0b5dbadc1
Khalid Hassan <khalidhassan218@outlook.com>
2024-08-23 14:46:07 +0300

    Your commit message

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
-import { gsap } from "gsap";
 import Slider from "./Sllider";
-import ShootingFire from "./ShootingFire";
 
 export default function Home() {
   return <Slider />;
